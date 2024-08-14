2fbb317f4aa853802283cebe6573edde84429e30
jdelpego <jdelpego2@gmail.com>
2024-08-14 12:45:55 +1000

    tested js

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,21 @@
-const replaceInnerHtml = (elementToReplace, content) => {
-    elementToReplace.innerHTML = content;
+// Function to toggle dark mode
+const toggleDark = () => {
+    document.body.classList.toggle('dark-mode');
+    document.body.getElementsByTagName('main')[0].classList.toggle('dark-mode');
+    
+    // Store the current state of dark mode in localStorage
+    const isDarkMode = document.body.classList.contains('dark-mode');
+    localStorage.setItem('darkMode', isDarkMode);
 };
 
-const showExperience = currentElemenet => {
-    let experienceContent = "";
-    replaceInnerHtml(currentElement, experienceContent);
+// Function to apply dark mode based on stored state
+const applyDarkMode = () => {
+    const darkMode = localStorage.getItem('darkMode');
+    if (darkMode === 'true') {
+        document.body.classList.add('dark-mode');
+        document.body.getElementsByTagName('main')[0].classList.add('dark-mode');
+    }
 };
 
-let headerElement = document.getElementsByTagName(header);
+// Apply dark mode on page load
+// window.onload = applyDarkMode;
